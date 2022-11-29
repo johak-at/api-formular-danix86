3192ab3c88f31672142c671dee67e886ac9b85ed
danix86 <daniel.daxer34@gmail.com>
2022-11-29 11:22:37 +0100

    dings

---
+++ src/components/StudentList.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+const studentList = ref([]);
+onMounted(async () => {
+    const response = await fetch('https://pb.seiwald.club/api/collections/4bhk/records')
+    const data = await response.json()
+    studentList.value = data.records
+})
+</script>
+
+<template>
+    <h1>Our Students</h1>
+    <ul>
+        <li v-for="student in studentList" :key="student.id">
+            {{ student.nachname }} {{ student.vorname }}
+        </li>
+    </ul>
+
+</template>
