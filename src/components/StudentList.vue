0c62f7ea6dd0697ef0c5a0c6fcafee32a91438ea
danix86 <daniel.daxer34@gmail.com>
2022-12-02 13:05:33 +0100

    did the thing

--- src/components/StudentList.vue
+++ src/components/StudentList.vue
@@ -1,18 +1,62 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 const studentList = ref([]);
+const vorname = ref('');
+const nachname = ref('');
+const wohnort = ref('');
+const geburtstag = ref('');
+const baseUrl = 'https://pb.seiwald.club/api/collections/4bhk/records';
 onMounted(async () => {
-    const response = await fetch('https://pb.seiwald.club/api/collections/4bhk/records')
-    const data = await response.json()
-    studentList.value = data.records
+    getStudents();
 })
+async function getStudents() {
+    const response = await fetch(baseUrl)
+    const data = await response.json();
+    studentList.value = data.items;
+}
+async function addStudent() {
+    const newStudent = {
+        vorname: vorname.value,
+        nachname: nachname.value,
+        wohnort: wohnort.value,
+        geburtstag: geburtstag.value
+    }
+    const response = await fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newStudent)
+    })
+    const data = await response.json();
+    studentList.value.push(data);
+    vorname.value = '';
+    nachname.value = '';
+    wohnort.value = '';
+    geburtstag.value = '';
+    getStudents();
+}
+async function deleteStudent(id) {
+    await fetch(`${baseUrl}/${id}`, {
+        method: 'DELETE'
+    })
+    getStudents();
+}
 </script>
 
 <template>
-    <h1>Our Students</h1>
+
+    <h1>Our Students:</h1>
+    <input type="text" v-model="vorname" placeholder="firstname">
+    <input type="text" v-model="nachname" placeholder="lastname">
+    <input type="text" v-model="wohnort" placeholder="wohnort">
+    <input type="date" v-model="geburtstag" placeholder="geburtstag">
+    <button @click.prevent="addStudent">Add Student</button>
     <ul>
         <li v-for="student in studentList" :key="student.id">
-            {{ student.nachname }} {{ student.vorname }}
+            {{ student.vorname }} {{ student.nachname }} wohnt in {{ student.wohnort }} und ist am
+            {{ student.geburtstag.substring(0, 10) }} geboren.
+            <button @click="deleteStudent(student.id)">X</button>
         </li>
     </ul>
 
